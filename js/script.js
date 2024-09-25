65d792f0ff2f7f8c8d12ceb75db13b90488f239e
nL013 <dinamit10d@gmail.com>
2024-09-26 00:16:05 +0500

    init

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function correctDate(date){
 
 
 function sale () {
-    let saleDate = new Date(2024, 8, 25)
+    let saleDate = new Date(2024, 8, 26)
     let currentDate = new Date()
     let date = saleDate - currentDate
     let day = Math.floor(date/(24*60*60*1000));
